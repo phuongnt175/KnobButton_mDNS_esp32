3cc43b06bd6b1d83dc89e740a501d4a0d4f714f1
phuongnt175 <phuong98a2@gmail.com>
2023-07-24 14:49:43 +0700

    fix subcribe payload mqtt

--- src/main.ino.cpp
+++ src/main.ino.cpp
@@ -1,29 +1,7 @@
-/*
- * Project ZX2D10GE01R-V4848 for Arduino
- * Description: The base project that can be used in the Arduino environment using the ZX2D10GE01R-V4848 device
- * Author: Eric Nam
- * Date: 03-18-2023
- * Custom by PhuongNT.
- * Last Change: 21-07-2023
- */
-
-// ** Prerequisites **
-// ESP32 Arduino 2.0.7 based on ESP-IDF 4.4.4
-// https://github.com/espressif/arduino-esp32
-
-// LVGL version 8.3.5
-// https://github.com/lvgl/lvgl
-
-// GFX Library for Arduino 1.3.2
-// https://github.com/moononournation/Arduino_GFX
-
-// ZX2D10GE01R-V4848 for ESP-IDF
-// https://github.com/wireless-tag-com/ZX2D10GE01R-V4848
-
-/******************************************************************************/
-/******************************************************************************/
-/*                              INCLUDE FILES                                 */
-/******************************************************************************/
+# 1 "C:\\Users\\admin1\\AppData\\Local\\Temp\\tmpjqqpxfjf"
+#include <Arduino.h>
+# 1 "C:/Users/admin1/Documents/PlatformIO/Projects/knobTest/src/main.ino"
+# 27 "C:/Users/admin1/Documents/PlatformIO/Projects/knobTest/src/main.ino"
 #include <lvgl.h>
 #include <Arduino_GFX_Library.h>
 #include "button.hpp"
@@ -35,9 +13,9 @@
 #include <PubSubClient.h>
 #include "esp_log.h"
 
-/******************************************************************************/
-/*                     PRIVATE TYPES and DEFINITIONS                         */
-/******************************************************************************/
+
+
+
 #define ECO_O(y) (y > 0) ? -1 : 1
 #define ECO_STEP(x) x ? ECO_O(x) : 0
 #define GFX_BL 38
@@ -47,22 +25,14 @@
 #define SERVICE_PORT 5600
 
 #define ADDRESS "172.16.100.137"
-
-/******************************************************************************/
-/*                     EXPORTED TYPES and DEFINITIONS                         */
-/******************************************************************************/
-
-
-/******************************************************************************/
-/*                              PRIVATE DATA                                  */
-/******************************************************************************/
-const char *MQTT_USER = "component"; // leave blank if no credentials used
-const char *MQTT_PASS = " "; // leave blank if no credentials used
+# 59 "C:/Users/admin1/Documents/PlatformIO/Projects/knobTest/src/main.ino"
+const char *MQTT_USER = "component";
+const char *MQTT_PASS = " ";
 const char *sw1topic = "component/bluetooth/control";
 
 WiFiManager wm;
-WiFiClientSecure  net;
-PubSubClient      client(net);
+WiFiClientSecure net;
+PubSubClient client(net);
 
 int PORT = 38883;
 bool res;
@@ -104,38 +74,38 @@ KENRFSPMPeyRyfzitR98wTtsORlF4I1+fYcPGSh0pQK1mK1X1bI/BWmtnRMqBSXD
 Eou01zV/f6o0PDqrnMlYhFi5gTg2bbqLYmLFgyw=
 -----END CERTIFICATE-----
 )=====";
-
-/******************************************************************************/
-/*                              EXPORTED DATA                                 */
-/******************************************************************************/
-
-/******************************************************************************/
-/*                            PRIVATE FUNCTIONS                               */
-/******************************************************************************/
+# 115 "C:/Users/admin1/Documents/PlatformIO/Projects/knobTest/src/main.ino"
 void mDNSService();
 void DemandWifi();
 void init_lv_group();
 
-/******************************************************************************/
-/*                            EXPORTED FUNCTIONS                              */
-/******************************************************************************/
 
-/******************************************************************************/
+
+
+
+
 Arduino_DataBus *bus = new Arduino_SWSPI(
-  GFX_NOT_DEFINED /* DC */, 21 /* CS */,
-  47 /* SCK */, 41 /* MOSI */, GFX_NOT_DEFINED /* MISO */);
+  GFX_NOT_DEFINED , 21 ,
+  47 , 41 , GFX_NOT_DEFINED );
 Arduino_ESP32RGBPanel *rgbpanel = new Arduino_ESP32RGBPanel(
-  39 /* DE */, 48 /* VSYNC */, 40 /* HSYNC */, 45 /* PCLK */,
-  10 /* R0 */, 16 /* R1 */, 9 /* R2 */, 15 /* R3 */, 46 /* R4 */,
-  8 /* G0 */, 13 /* G1 */, 18 /* G2 */, 12 /* G3 */, 11 /* G4 */, 17 /* G5 */,
-  47 /* B0 */, 41 /* B1 */, 0 /* B2 */, 42 /* B3 */, 14 /* B4 */,
-  1 /* hsync_polarity */, 10 /* hsync_front_porch */, 10 /* hsync_pulse_width */, 10 /* hsync_back_porch */,
-  1 /* vsync_polarity */, 14 /* vsync_front_porch */, 2 /* vsync_pulse_width */, 12 /* vsync_back_porch */);
+  39 , 48 , 40 , 45 ,
+  10 , 16 , 9 , 15 , 46 ,
+  8 , 13 , 18 , 12 , 11 , 17 ,
+  47 , 41 , 0 , 42 , 14 ,
+  1 , 10 , 10 , 10 ,
+  1 , 14 , 2 , 12 );
 Arduino_RGB_Display *gfx = new Arduino_RGB_Display(
-  480 /* width */, 480 /* height */, rgbpanel, 0 /* rotation */, true /* auto_flush */,
-  bus, GFX_NOT_DEFINED /* RST */, st7701_type7_init_operations, sizeof(st7701_type7_init_operations));
-
-/* Display flushing */
+  480 , 480 , rgbpanel, 0 , true ,
+  bus, GFX_NOT_DEFINED , st7701_type7_init_operations, sizeof(st7701_type7_init_operations));
+void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p);
+void encoder_read(lv_indev_drv_t *drv, lv_indev_data_t *data);
+void connectBroker();
+void Callback(char* topic, byte* payload, unsigned int length);
+void setup();
+void loop();
+void ui_event_resetWifi(lv_event_t * e);
+void ui_event_button3(lv_event_t * e);
+#line 139 "C:/Users/admin1/Documents/PlatformIO/Projects/knobTest/src/main.ino"
 void my_disp_flush(lv_disp_drv_t *disp, const lv_area_t *area, lv_color_t *color_p) {
   uint32_t w = (area->x2 - area->x1 + 1);
   uint32_t h = (area->y2 - area->y1 + 1);
@@ -172,7 +142,7 @@ void connectBroker()
     {
       ESP_LOGE(TAG, "Connected");
       client.subscribe(sw1topic);
-      //client.subscribe(sw2topic);
+
     }
     else
     {
@@ -186,7 +156,7 @@ void connectBroker()
 
 void Callback(char* topic, byte* payload, unsigned int length) {
 
-  payload[length] = '\0'; //NULL terminator used to terminate the char array
+  payload[length] = '\0';
   String message = (char*)payload;
   if(String(topic) == sw1topic)
   {
@@ -197,7 +167,7 @@ void Callback(char* topic, byte* payload, unsigned int length) {
       btnStatus1 = "ON";
       if(lv_obj_get_state(ui_Button3) == 6)
       {
-        _ui_state_modify(ui_Button3, LV_STATE_CHECKED, 2);// _UI_STATE_MODIFY_TOGGLE
+        _ui_state_modify(ui_Button3, LV_STATE_CHECKED, 2);
         ESP_LOGE(TAG, "Btn status: %d", lv_obj_get_state(ui_Button3));
         ESP_LOGE(TAG, "ON");
       }
@@ -206,7 +176,7 @@ void Callback(char* topic, byte* payload, unsigned int length) {
     {
       btnStatus1 = "OFF";
       ESP_LOGE(TAG, "Btn status: %d", lv_obj_get_state(ui_Button3));
-      _ui_state_modify(ui_Button3, LV_STATE_CHECKED, 1);// _UI_STATE_MODIFY_TOGGLE
+      _ui_state_modify(ui_Button3, LV_STATE_CHECKED, 1);
       ESP_LOGE(TAG, "Btn status: %d", lv_obj_get_state(ui_Button3));
       ESP_LOGE(TAG, "OFF");
     }
@@ -224,34 +194,34 @@ void setup() {
 
   lv_init();
 
-  // Hardware Button
+
   g_btn = button_attch(3, 0, 10);
 
-  // Magnetic Encoder
+
   mt8901_init(5, 6);
 
   screenWidth = gfx->width();
   screenHeight = gfx->height();
 
-  // Must use PSRAM
+
   disp_draw_buf = (lv_color_t *)heap_caps_malloc(sizeof(lv_color_t) * screenWidth * 32, MALLOC_CAP_INTERNAL | MALLOC_CAP_8BIT);
 
   if (!disp_draw_buf) {
-    //Serial.println("LVGL disp_draw_buf allocate failed!");
+
     ESP_LOGE(TAG, "LVGL disp_draw_buf allocate failed!");
   } else {
     lv_disp_draw_buf_init(&draw_buf, disp_draw_buf, NULL, screenWidth * 32);
 
-    /* Initialize the display */
+
     lv_disp_drv_init(&disp_drv);
-    /* Change the following line to your display resolution */
+
     disp_drv.hor_res = screenWidth;
     disp_drv.ver_res = screenHeight;
     disp_drv.flush_cb = my_disp_flush;
     disp_drv.draw_buf = &draw_buf;
     lv_disp_drv_register(&disp_drv);
 
-    /* Initialize the input device driver */
+
     static lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.read_cb = encoder_read;
@@ -264,7 +234,7 @@ void setup() {
   ESP_LOGE(TAG, "%d.%d.%d.%d ", WiFi.localIP()&0xFF, (WiFi.localIP()>>8)&0xFF, (WiFi.localIP()>>16)&0xFF, (WiFi.localIP()>>24)&0xFF);
 
   if(!MDNS.begin("esp32")) {
-    //Serial.println("Error starting mDNS");
+
     ESP_LOGE(TAG, "error starring mDNS!");
     return;
   }
@@ -283,17 +253,17 @@ void setup() {
 }
 
 void loop() {
-  lv_timer_handler(); /* let the GUI do its work */
+  lv_timer_handler();
 
   if(!client.connected())
   {
-    client.setKeepAlive(60); // setting keep alive to 60 seconds
+    client.setKeepAlive(60);
     connectBroker();
   }
   else{
     client.loop();
   }
-  //ESP_LOGE(TAG, "%d", lv_obj_get_state(ui_Button3));
+
   delay(5);
 }
 
@@ -318,7 +288,7 @@ void ui_event_resetWifi(lv_event_t * e)
 {
     lv_event_code_t event_code = lv_event_get_code(e);
     lv_obj_t * target = lv_event_get_target(e);
-    if(event_code == LV_EVENT_CLICKED) //detect clicked, run the DemandWiFi function
+    if(event_code == LV_EVENT_CLICKED)
     {
         DemandWifi();
     }
@@ -326,8 +296,8 @@ void ui_event_resetWifi(lv_event_t * e)
 
 void DemandWifi()
 {
-  wm.setConfigPortalTimeout(300); //set timeout 300s
- 
+  wm.setConfigPortalTimeout(300);
+
   if (!wm.startConfigPortal("OnDemandAP","LumiVn@2023")) {
     delay(5000);
     MDNS.begin("esp32");
@@ -339,34 +309,34 @@ void mDNSService()
   MDNS.addService(SERVICE_NAME, SERVICE_PROTOCOL, SERVICE_PORT);
 
   int nrOfServices = MDNS.queryService(SERVICE_NAME, SERVICE_PROTOCOL);
-  
+
   if (nrOfServices == 0) {
-    //Serial.println("No services were found.");
+
     ESP_LOGE(TAG, "No services were found.");
-  } 
+  }
   else {
-    
-    //Serial.print("Number of services found: ");
+
+
     ESP_LOGE(TAG, "Number of services found: %d", nrOfServices);
-      
-    for (int i = 0; i < nrOfServices; i=i+1) 
+
+    for (int i = 0; i < nrOfServices; i=i+1)
     {
-      //Serial.println("---------------");
+
       ESP_LOGE(TAG, "---------------");
-        
-      //Serial.print("Hostname: ");
-      //Serial.println(MDNS.hostname(i));
+
+
+
       ESP_LOGE(TAG, "Hostname: %s", MDNS.hostname(i));
 
-      //Serial.print("IP address: ");
-      //Serial.println(MDNS.IP(i));
+
+
       ESP_LOGE(TAG, "IP address: %d.%d.%d.%d", MDNS.IP(i)&0xFF, (MDNS.IP(i)>>8)&0xFF, (MDNS.IP(i)>>16)&0xFF, (MDNS.IP(i)>>24)&0xFF);
 
-      //Serial.print("Port: ");
-      //Serial.println(MDNS.port(i));
+
+
       ESP_LOGE(TAG, "Port: %d", MDNS.port(i));
 
-      //Serial.println("---------------");
+
       ESP_LOGE(TAG, "---------------");
     }
   }
@@ -376,7 +346,7 @@ void ui_event_button3(lv_event_t * e)
 {
     lv_event_code_t event_code = lv_event_get_code(e);
     lv_obj_t * target = lv_event_get_target(e);
-    if(event_code == LV_EVENT_VALUE_CHANGED &&  lv_obj_has_state(target, LV_STATE_CHECKED)) {
+    if(event_code == LV_EVENT_VALUE_CHANGED && lv_obj_has_state(target, LV_STATE_CHECKED)) {
       if(btnStatus1 = "OFF")
       {
         client.publish(sw1topic, "ON");
@@ -384,7 +354,7 @@ void ui_event_button3(lv_event_t * e)
         ESP_LOGE(TAG, "button ON");
       }
     }
-    if(event_code == LV_EVENT_VALUE_CHANGED &&  !lv_obj_has_state(target, LV_STATE_CHECKED)) {
+    if(event_code == LV_EVENT_VALUE_CHANGED && !lv_obj_has_state(target, LV_STATE_CHECKED)) {
       if(btnStatus1 == "ON")
       {
         client.publish(sw1topic, "OFF");
